0b6ee7b5d96d522a75b0179257586452e729a908
mwingerden <matthew.wingerden@gmail.com>
2024-11-06 16:48:30 -0800

    Added inventory management.

---
+++ Inventory/Scripts/inv_ui.gd
@@ -0,0 +1,31 @@
+extends Control
+
+@onready var inv: Inv = preload("res://Inventory/Scenes/playerinv.tres")
+@onready var slots: Array = $NinePatchRect/GridContainer.get_children()
+
+var is_open = false
+
+func _ready():
+	SignalManager.update.connect(update_slots)
+	update_slots()
+	close()
+
+func update_slots():
+	for i in range(min(inv.slots.size(), slots.size())):
+		slots[i].update(inv.slots[i])
+
+@warning_ignore("unused_parameter")
+func _process(delta):
+	if Input.is_action_just_pressed("i"):
+		if is_open:
+			close()
+		else:
+			open()
+
+func open():
+	visible = true
+	is_open = true
+
+func close():
+	visible = false
+	is_open = false
--- Scripts/Player/alex.gd
+++ Scripts/Player/alex.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @export var speed = 700
+@export var inv: Inv
 
 func get_input():
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -10,3 +11,6 @@ func get_input():
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
+
+func collect(item):
+	inv.insert(item)
